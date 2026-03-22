b1491f55dabcd71ccb5303c819104069fa8c120d
anass17 <anassboutaib2018@gmail.com>
2026-03-22 22:43:45 +0100

    fix CI pipeline

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,13 +2,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    db_user: str
-    db_password: str
-    db_host: str
-    db_port: int
-    db_name: str
+    db_user: str = "user"
+    db_password: str = "password"
+    db_host: str = "host"
+    db_port: int = "5432"
+    db_name: str = "fluxia"
 
-    secret_key: str
+    secret_key: str = "fluxia_123456789"
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
 
