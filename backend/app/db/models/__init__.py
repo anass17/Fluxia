bd3ca77715c064a87ff58604b4191913e29f8ee6
anass17 <anassboutaib2018@gmail.com>
2026-03-22 05:07:45 +0100

    Add Diagrams

--- backend/app/db/models/__init__.py
+++ backend/app/db/models/__init__.py
@@ -2,3 +2,4 @@ from app.db.models.user import User
 from app.db.models.reservation import Reservation
 from app.db.models.plate import Plate
 from app.db.models.action import Action
+from app.db.models.order import Order 
---
+++ backend/app/db/models/order.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, TIMESTAMP, ForeignKey, func
+from app.db.base import Base
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True, index=True)
+    quantity = Column(Integer, nullable=False)
+    reservation_id = Column(Integer, ForeignKey("reservations.id"), nullable=False)
+    plate_id = Column(Integer, ForeignKey("plates.id"), nullable=False)
+    created_at = Column(TIMESTAMP, server_default=func.now())
